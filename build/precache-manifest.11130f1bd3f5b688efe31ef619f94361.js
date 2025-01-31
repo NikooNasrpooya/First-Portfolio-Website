5b51a3ac4a10fae99e1d8a180db0c55ab965efb6
Nikoo Nasrpooya <nikoonasrpooya@Nikoos-MacBook-Pro.local>
2025-01-31 15:22:54 -0500

    First Portfolio Website

---
+++ build/precache-manifest.11130f1bd3f5b688efe31ef619f94361.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff72233bcb4b562d6b9c305fd3410361",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "eda5289b364789d6245a",
+    "url": "/portfolio-website/static/css/main.b5d1ba6e.chunk.css"
+  },
+  {
+    "revision": "f56430b159cd08ef03e4",
+    "url": "/portfolio-website/static/js/2.cc0bc21e.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/portfolio-website/static/js/2.cc0bc21e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eda5289b364789d6245a",
+    "url": "/portfolio-website/static/js/main.d9a6d0f8.chunk.js"
+  },
+  {
+    "revision": "9acb84acd13b69e2bf79",
+    "url": "/portfolio-website/static/js/runtime-main.bc6b0371.js"
+  },
+  {
+    "revision": "c3cc6db2220c8f3fa3f4ad8644d9cf40",
+    "url": "/portfolio-website/static/media/CSSGrid.c3cc6db2.jpg"
+  },
+  {
+    "revision": "cf71a98c6bc707f2d070388c7afcb0c9",
+    "url": "/portfolio-website/static/media/css.cf71a98c.png"
+  },
+  {
+    "revision": "9a073b4cb55995549f7dc759eb46f831",
+    "url": "/portfolio-website/static/media/git.9a073b4c.png"
+  },
+  {
+    "revision": "226496ac85e2e5645046159d09546edc",
+    "url": "/portfolio-website/static/media/github.226496ac.png"
+  },
+  {
+    "revision": "7c64d4f7ea5070f290427a97f27b151f",
+    "url": "/portfolio-website/static/media/graphql.7c64d4f7.png"
+  },
+  {
+    "revision": "5b9e57e9aadbdb82ad1a5ed649cac8fc",
+    "url": "/portfolio-website/static/media/heroImage.5b9e57e9.png"
+  },
+  {
+    "revision": "a80a90015e4a63d3cf6a71875f27937e",
+    "url": "/portfolio-website/static/media/html.a80a9001.png"
+  },
+  {
+    "revision": "dbf4a3c9a8a3dbbb6743c7c339c05265",
+    "url": "/portfolio-website/static/media/installNode.dbf4a3c9.jpg"
+  },
+  {
+    "revision": "68777e9738cf44a28fe3db67e269f224",
+    "url": "/portfolio-website/static/media/javascript.68777e97.png"
+  },
+  {
+    "revision": "b149a474d80688b9fee45b951c593522",
+    "url": "/portfolio-website/static/media/navbar.b149a474.jpg"
+  },
+  {
+    "revision": "a2488252f4d424bb4ea89bf574ccf0a4",
+    "url": "/portfolio-website/static/media/nextjs.a2488252.png"
+  },
+  {
+    "revision": "4380e99d266b735f5a05be5b9cd8ccdb",
+    "url": "/portfolio-website/static/media/node.4380e99d.png"
+  },
+  {
+    "revision": "8eacb570fdccc669f09690b4c2918b00",
+    "url": "/portfolio-website/static/media/python.8eacb570.png"
+  },
+  {
+    "revision": "1b28808441932b7571626b4edcc307fe",
+    "url": "/portfolio-website/static/media/react.1b288084.png"
+  },
+  {
+    "revision": "6c0ea41b8ddfe867f36e5c0f9ac8c5f7",
+    "url": "/portfolio-website/static/media/reactSmooth.6c0ea41b.jpg"
+  },
+  {
+    "revision": "ef9618a31a9746ff812bd77227690d18",
+    "url": "/portfolio-website/static/media/reactWeather.ef9618a3.jpg"
+  },
+  {
+    "revision": "4d88ca033fd854136a7695046e8910d4",
+    "url": "/portfolio-website/static/media/tailwind.4d88ca03.png"
+  },
+  {
+    "revision": "c19f1fb586c8d704c53a2dd68388c3b2",
+    "url": "/portfolio-website/static/media/toDoList.c19f1fb5.png"
+  },
+  {
+    "revision": "2e432644764658516118ceb358dce041",
+    "url": "/portfolio-website/static/media/visualstudio.2e432644.png"
+  }
+]);
